d55c0f01a08e68540f693564e710a9ba600a6849
Micah <Micah@192.168.0.192>
2014-02-14 10:36:22 -0600

    Added explanatory comments.

--- SimpleList/src/com/example/simplelist/MainActivity.java
+++ SimpleList/src/com/example/simplelist/MainActivity.java
@@ -14,18 +14,32 @@ import android.widget.ListView;
 
 public class MainActivity extends Activity {
 
+	//1.  For any list, I need a ListView, and an adapter to go with it...
 	public ListView listView;
 	public SimpleListAdapter adapter;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-			
+		
+		//2.  Set the content view to a particular layout file.
+		setContentView(R.layout.activity_main);			
+		
+		//3.  Now I need to initiate the listView variable, and actually assign it to a list view.  I do this with "findViewById".  I can run this here, 
+		//because my MainActivity class extends Activity, which comes with a "findViewById" method.  This activity knows what R.id.listView1 is, because 
+		//R.id.listView1 is a child of the R.layout.activity_main file, which is this Activity's current layout. 
 		listView = (ListView)findViewById(R.id.listView1);
+		
+		//4.  Also, I make the background black because its easier to see...
 		listView.setBackgroundColor(Color.BLACK);
+		
+		//5.  Now, I need to initiate the adapter to go with my list view.  It needs a reference to the context, so I give it that.
 		adapter = new SimpleListAdapter(getApplicationContext());
+		
+		//6.  Once I create the adapter, I have to actually pair it up with the listView...
 		listView.setAdapter(adapter);		
+		
+		//7.  Now, I run a special method which populates the list.  Go down to getPics() to see more detail.
 		getPics();
 			
 	}
@@ -41,19 +55,36 @@ public class MainActivity extends Activity {
 
 	private void getPics(){
 		
+		
+		//1.  So the goal here is to read all of the pictures out of a certain directory, and use them to create list items that then go into my listView.  So, step 1 is 
+		//I go in and designate the directory I want to pull data out of...
 		File sdDir = new File("/sdcard/DCIM/Camera");
+		
+		//2.  Now, I make an array of all of the files in that directory.		
 		File[] sdDirFiles = sdDir.listFiles();
+		
+		//3.  Now, for each file in that directory, I will do something...
 		for(File singleFile : sdDirFiles)
 		{		   
 			
+		  //4.  For each file in the directory, I am going to create a "SimpleItem".  This is a class I have created, and all it is is a thumbnail of the picture,
+		  //a String for the file name, and another String for the file size.  So, first I will create the thumbnail.  The Drawable class has a method that lets
+		  //me do that, given a file path, so I run that for each file...
 		  Drawable d = Drawable.createFromPath(singleFile.getAbsolutePath());
+		  
+		  //5.  Next, I get the name and file size of the file.
 		  String name = singleFile.getName();
 		  String size = String.valueOf(singleFile.length());
 		 
+		  //6.  Now, I take this data, and put it into a new SimpleItem that I have constructed.
 		  SimpleItem i = new SimpleItem();
 		  i.name = name;
 		  i.size = size;
 		  i.pic = d;
+		  
+		  //7.  Finally, I add this new item to the ListAdapter.  In there is where the real work happens.  I have two ways of adding items.  Just "adapter.add(SimpleItem i)" 
+		  //will add the item, and make the list refresh itself.  Or, if you want to add a bunch of items first and only refresh the list once, 
+		  //you can use "adapter.addButDontUpdate(SimpleItem i)", and then later run adapter.updateManually().  Now, go over to SimpleListAdapter to see how this works.
 		  
 		  adapter.addButDontUpdate(i);
 		  
--- SimpleList/src/com/example/simplelist/SimpleListAdapter.java
+++ SimpleList/src/com/example/simplelist/SimpleListAdapter.java
@@ -15,13 +15,27 @@ import android.widget.TextView;
 
 public class SimpleListAdapter extends BaseAdapter{
 
+	
+	//1.  So, first of all, the adapter needs to keep a copy of all of the things that it is managing.  One way to think of a "thing" that the adapter is 
+	//managing is that each "thing" is just a collection of the data that will go into a single element on the list.  In this case, each "thing" being managed is 
+	//a SimpleItem, which is a class I defined.  The only reason I defined the class was as a container for the three fields of Picture, filename and filesize.  In any event,
+	//you will want the adapter to keep a list of Type <Class> where Class is a class of items that represent all the stuff that will go into a single list item. 
+	//So, we do this here.  Also, we will need a reference to the application's context, so we have that too...
 	ArrayList<SimpleItem> items;
 	Context mContext;
 		
+	//2.  So, let's say I want to add a SimpleItem to my ListView. What I actually do is add it to my adapter, and then the adapter refreshes the ListView, rebuilding it
+	//with all of the things it contains (including the thing I just added).  The adding of an item to the list of items the adapter is managing is done by the items.add command.
+	//Then, the notifyDataSetChanged method will automatically call some other stuff in order to rebuild the ListView, one list item at a time.
+	
+	//So, addItem(item) does that.
 	public void addItem(SimpleItem item){
 		items.add(item);
 		notifyDataSetChanged();
 	}
+	
+	//3.  As I mentioned, in the MainActivity.java comments, I also split apart the adding of an item and list refreshing into two methods, so that you can load a bunch
+	//of items and then only refresh the list once, if you wish.  That is the purpose of addButDontUpdate(SimpleItem item) and updateManually().
 	
 	public void addButDontUpdate(SimpleItem item){
 		items.add(item);
@@ -31,44 +45,67 @@ public class SimpleListAdapter extends BaseAdapter{
 		notifyDataSetChanged();
 	}
 	
+	
+	//4. Here is the adapter's constructor.  Again, we need to get the context of the application, so this ensures that we do.
 	public SimpleListAdapter(Context context){
 		mContext = context;
 		items = new ArrayList<SimpleItem>();
 	}
 	
+	
+	//5.  Now, I think what happens is that when you call notifyDataSetChanged, it iterates through your list, and builds a new View for each list item.
+	//How does it know how to iterate through the list?  I suspect it is through the following three methods... getCount(), getItem(int position), getItemId(int position).
+	//Anyway, they just need to do the obvious things you would expect of them...
+	
+			//getCount needs to return the size of your list (probably because there is a "for (int i=0; i<adapter.getCount(); i++)" somewhere in notifyDataSetChanged...
 	@Override
 	public int getCount() {
 		// TODO Auto-generated method stub
 		return items.size();
 	}
 
+			//getItem needs to, given a certain position in the list, return the item at that position.  Again, notifyDataSetChanged probably has a
+			//"Object foo = adapter.getItem(i);" somewhere, being used to pull up your items as it iterates through all the positions.
 	@Override
 	public Object getItem(int position) {
 		
 		return items.get(position);
 	}
 
+			//getItemId returns a unique ID associated with each item... maybe to prevent list duplicates if you want?  I'm not sure, but I just return back position as the ID and
+			//that works.
 	@Override
 	public long getItemId(int position) {
 		// TODO Auto-generated method stub
 		return position;
 	}
 
+	
+	//6.  Finally, there is the getView method.  Here is where the real work of the adapter comes in.	
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		
+		//So, first of all, we are going to create a new View, which will represent one item on our list.  What does View look like?  Well, it looks like
+		//R.layout.listitem.  So, we create the java object itemView using LayoutInflater.
 		View itemView = LayoutInflater.from(mContext).inflate(R.layout.listitem, null);
-		TextView et = (TextView)itemView.findViewById(R.id.TextView1);
-		et.setText(items.get(position).name);
-		TextView et2 = (TextView)itemView.findViewById(R.id.TextView2);
-		et2.setText(items.get(position).size);
+		
+		//Now that itemView exists, and since it corresponds to the R.layout.listitem, we can count on finding some other views as children of it
+		//(because we build R.layout.listitem to contain precisely those views).  So, we find the child views, and give them names...
+		TextView et = (TextView)itemView.findViewById(R.id.TextView1);		
+		TextView et2 = (TextView)itemView.findViewById(R.id.TextView2);		
 		ImageView image = (ImageView) itemView.findViewById(R.id.imageView1);
 		
+		//Now, we fill our child views with the appropriate data.  What data?  Data corresponding to the fields of a SimpleItem.  Which SimpleItem?  The one in 
+		//spot number "position" in our list "items".
+		et.setText(items.get(position).name);
+		et2.setText(items.get(position).size);
 		
+		//This stuff is to create the picture for the ImageView in our list, and resize it a bit.
 		Bitmap b = ((BitmapDrawable)items.get(position).pic).getBitmap();	
 		Bitmap bitmapResized = Bitmap.createScaledBitmap(b, 100, 100, false); 
 		image.setImageDrawable(new BitmapDrawable(mContext.getResources(), bitmapResized));
 		
+		//Finally, we return this particular view, now all constructed and populated.
 		return itemView;
 	}
 
